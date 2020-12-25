c71e64792cd98bd0ed71429f53674917e3dbda65
elect <elect@DESKTOP-63OAS16>
2020-12-25 11:25:13 -0800

    Add Environment vars

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("publid"));
@@ -11,10 +13,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/secretDB", {useNewUrlParser:true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema( {
     email:String,
     password:String
-};
+});
+
+userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -77,6 +81,6 @@ app.post("/login", function(req, res){
 
 
 
-app.listen(3000, function(){
-    console.log("Server running on port 3000");
+app.listen(3001, function(){
+    console.log("Server running on port 3001");
 });
